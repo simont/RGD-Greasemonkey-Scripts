e6db12f989aa289c469565caf9ae8dcab5c6bc3c
Simon Twigger <simont@mcw.edu>
2010-08-17 12:54:42 -0500

    add gene page script

---
+++ gm_scripts/rgd-gene-ratmine/rgd-gene-ratmine.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name           RGD-GENE-RatMine
+// @namespace      http://github.com/simont/RGD-Greasemonkey-Scripts
+// @description    Adds ratmine code to RGD Gene Reports
+// @include        http://rgd.mcw.edu/tools/genes/*
+// ==/UserScript==
