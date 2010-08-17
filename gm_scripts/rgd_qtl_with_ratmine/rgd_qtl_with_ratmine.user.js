4c8510d443ce16596849b8a58feafef52fc85b20
Simon Twigger <simont@mcw.edu>
2010-08-17 11:40:39 -0500

    initial commit

---
+++ gm_scripts/rgd_qtl_with_ratmine/rgd_qtl_with_ratmine.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name           RGD_QTL_with_RatMine
+// @namespace      http://github.com/simont/RGD-Greasemonkey-Scripts
+// @description    Adding RatMine links to RGD QTL pages
+// @include        http://rgd.mcw.edu/objectSearch/qtlReport.jsp
+// ==/UserScript==
