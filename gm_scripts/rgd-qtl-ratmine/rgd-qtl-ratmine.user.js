1a2817b9e1172393ed587048e5713ee71a7f0de9
Simon Twigger <simont@mcw.edu>
2010-08-17 11:50:04 -0500

    basic script

---
+++ gm_scripts/rgd-qtl-ratmine/rgd-qtl-ratmine.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           RGD-QTL-RatMine
+// @namespace      http://github.com/simont/RGD-Greasemonkey-Scripts
+// @description    Adds RatMine links to RGD QTL page
+// @include        http://rgd.mcw.edu/objectSearch/*
+// ==/UserScript==
+
+alert("Hello, world!");
